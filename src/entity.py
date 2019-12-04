3eda1d43af3bf815c3acf97cd25185dcb3ab382c
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-12-04 15:22:55 -0600

    room class

--- src/entity.py
+++ src/entity.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 
 class Entity(ABC):
 
-    def __init__(self):
-        self.contains = []
+    def __init__(self, name):
+        self.name = name
+        self.contents = []
 
     @abstractmethod
     def look(self):
--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+from entity import Entity
+
+class Room(Entity):
+    def __init__(self, name, description):
+        super().__init__(name)
+        self.description = description
+        self.north = None
+        self.south = None
+        self.east = None
+        self.west = None
+
+    def look(self):
+        content_list = '\n'.join(' - ' + item.name for item in self.contents)
+        return self.description + '\n\nRoom contains:\n' + content_list
