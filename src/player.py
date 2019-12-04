589ae616d50978fb3935073adaebbd77131942bf
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-12-04 15:45:51 -0600

    player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from entity import Entity
+
+class player(Entity):
+    def __init__(self, name, gender):
+        super().__init__(name)
+        self.gender = gender
+        self.eyes = 2
+        self.hominid_arms = 2
+        self.hominid_legs = 2
+        self.tails = 0
